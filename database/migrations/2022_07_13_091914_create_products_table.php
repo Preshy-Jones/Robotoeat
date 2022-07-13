b79002bbb963c4fe4e4112151ef2092817d66629
Precious Adedibu <adedibuprecious@gmail.com>
2022-07-13 23:54:32 +0100

    set up migrations an db

---
+++ database/migrations/2022_07_13_091914_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->text('product_description');
+            $table->boolean('isLiked')->default(False);
+            $table->text('product_name');
+            $table->text('product_image_url');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    
+
+    {{-- @vite(['resources/js/app.js']) --}}
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/7f0efd52ac.js" crossorigin="anonymous"></script>
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script> --}}
+    <!-- Styles -->
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div>
+        @include('inc.navbar')   
+        <div class="container">
+            @yield('content')
+        </div>
+        @include('inc.footer')   
+    </div>
+</body>
+</html>
+
+
+</body>
+</html>
+</body>
+</html>
